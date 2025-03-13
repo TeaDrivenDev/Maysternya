ed2b94a0e93c2ab68bf0785429cc46c33ea79f4c
TeaDrivenDev <TeaDrivenDev@gmail.com>
2025-03-13 21:45:55 +0100

    Add string constants

--- src/Maysternya/Domain.fs
+++ src/Maysternya/Domain.fs
@@ -1,5 +1,37 @@
 ﻿namespace Maysternya.Domain
 
+[<RequireQualifiedAccess>]
+module Constants =
+    [<Literal>]
+    let WorkshopContentPath = @"steamapps\workshop\content"
+
+    [<Literal>]
+    let Ets2Id = "227300"
+
+    [<Literal>]
+    let AtsId = "270880"
+
+    [<Literal>]
+    let VersionsSii = "versions.sii"
+
+    [<Literal>]
+    let ManifestSii = "manifest.sii"
+
+    [<Literal>]
+    let SiiNunit = "SiiNunit"
+
+    [<Literal>]
+    let DisplayNameKey = "display_name"
+
+    [<Literal>]
+    let PackageVersionInfoKey = "package_version_info"
+
+    [<Literal>]
+    let PackageNameKey = "package_name"
+
+    [<Literal>]
+    let CompatibleVersionsKey = "compatible_versions[]"
+
 type ConfiguredDirectory =
     {
         Path: string
