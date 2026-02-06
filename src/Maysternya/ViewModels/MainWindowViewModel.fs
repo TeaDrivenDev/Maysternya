5228554bb468a919e93f8c79f775404bb7f08b3c
TeaDrivenDev <TeaDrivenDev@gmail.com>
2026-02-06 02:18:29 +0100

    Show end-user message about directory status

--- src/Maysternya/ViewModels/MainWindowViewModel.fs
+++ src/Maysternya/ViewModels/MainWindowViewModel.fs
@@ -9,20 +9,50 @@ open App
 type MainWindowViewModel(folderPicker: Services.FolderPickerService) as this =
     inherit ReactiveElmishViewModel()
 
+    // TODO Probably move these elsewhere
+    let workshopDirectoryButNoModsMessage = "Workshop directory found, but no ETS2 or ATS mod directories"
+    let noWorkshopDirectoryMessage = "Workshop directory not found"
+
+    let ets2ModsDirectoryFoundMessage = "ETS2 mod directory found"
+    let noEts2ModsDirectoryFoundMessage = "ETS2 mod directory not found"
+
+    let atsModsDirectoryFoundMessage = "ATS mod directory found"
+    let noAtsModsDirectoryFoundMessage = "ATS mod directory not found"
+
     member this.SteamDirectory
         with get () = this.Bind(store, _.SteamDirectory.Path)
         and set value = store.Dispatch(UpdateSteamDirectory (Some value))
 
     member this.IsSteamDirectoryValid: bool = this.Bind(store, _.SteamDirectory.PathExists)
 
-    member this.WorkshopDirectory = this.Bind(store, _.WorkshopDirectory.Path)
-    member this.IsWorkshopDirectoryValid = this.Bind(store, _.WorkshopDirectory.PathExists)
+    member this.WorkshopDirectoryMessage =
+        this.Bind(
+            store,
+            fun model ->
+                if not model.WorkshopDirectory.PathExists
+                then noWorkshopDirectoryMessage
+                elif not model.Ets2ModsDirectory.PathExists && not model.AtsModsDirectory.PathExists
+                then workshopDirectoryButNoModsMessage
+                else "")
 
-    member this.Ets2ModsDirectory = this.Bind(store, _.Ets2ModsDirectory.Path)
-    member this.IsEts2ModsDirectoryValid = this.Bind(store, _.Ets2ModsDirectory.PathExists)
+    member this.IsShowWorkshopDirectoryMessage =
+        this.Bind(store, fun model -> this.WorkshopDirectoryMessage <> "")
 
-    member this.AtsModsDirectory = this.Bind(store, _.AtsModsDirectory.Path)
-    member this.IsAtsModsDirectoryValid = this.Bind(store, _.AtsModsDirectory.PathExists)
+    member this.Ets2ModsDirectoryMessage =
+        this.Bind(
+            store,
+            fun model ->
+                if model.Ets2ModsDirectory.PathExists
+                then ets2ModsDirectoryFoundMessage
+                else noEts2ModsDirectoryFoundMessage)
+
+    member this.AtsModsDirectoryMessage =
+        this.Bind(
+            store,
+            fun model ->
+                if model.AtsModsDirectory.PathExists
+                then atsModsDirectoryFoundMessage
+                else noAtsModsDirectoryFoundMessage)
 
     member this.SelectSteamDirectory() =
         task {
