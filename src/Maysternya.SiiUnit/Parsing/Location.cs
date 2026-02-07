fb5079117af853bf0499bebaa49a9d9a6047fba8
TeaDrivenDev <TeaDrivenDev@gmail.com>
2026-02-07 05:19:01 +0100

    Make Location record insted of class

--- src/Maysternya.SiiUnit/Parsing/Location.cs
+++ src/Maysternya.SiiUnit/Parsing/Location.cs
@@ -1,15 +1,3 @@
 ﻿namespace Maysternya.SiiUnit.Parsing;
 
-public sealed class Location
-{
-    internal Location(int line, int column, int offset)
-    {
-        this.Line = line;
-        this.Column = column;
-        this.Offset = offset;
-    }
-
-    public int Line { get; }
-    public int Column { get; }
-    public int Offset { get; }
-}
+public sealed record Location(int Line, int Column, int Offset);
