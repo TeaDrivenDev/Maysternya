03d33ef04ec80b5693a1d00aec20f8a1199b2125
TeaDrivenDev <TeaDrivenDev@gmail.com>
2026-02-11 21:15:42 +0100

    Delete obsolete string constants

--- src/Maysternya/Domain.fs
+++ src/Maysternya/Domain.fs
@@ -30,47 +30,6 @@ module Constants =
         [<Literal>]
         let ManifestSii = "manifest.sii"
 
-    module PackageFileKeys =
-        [<Literal>]
-        let SiiNunit = "SiiNunit"
-
-        [<Literal>]
-        let DisplayName = "display_name"
-
-        [<Literal>]
-        let PackageVersionInfo = "package_version_info"
-
-        [<Literal>]
-        let PackageName = "package_name"
-
-        [<Literal>]
-        let CompatibleVersions = "compatible_versions[]"
-
-        [<Literal>]
-        let Informational = "informational"
-
-    module ManifestFileKeys =
-        [<Literal>]
-        let ModPackage = "mod_package"
-
-        [<Literal>]
-        let PackageVersion = "package_version"
-
-        [<Literal>]
-        let DisplayName = "display_name"
-
-        [<Literal>]
-        let Author = "author"
-
-        [<Literal>]
-        let Icon = "icon"
-
-        [<Literal>]
-        let DlcDependencies = "dlc_dependencies[]"
-
-        [<Literal>]
-        let MpModOptional = "mp_mod_optional"
-
 type ConfiguredDirectory =
     {
         Path: string
