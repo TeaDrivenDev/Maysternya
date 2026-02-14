e2b28ea1916b7ea13cc518b358da83be60b9d13e
TeaDrivenDev <TeaDrivenDev@gmail.com>
2026-02-14 01:12:08 +0100

    Only show button in rows with version restriction

--- src/Maysternya/UIUtilities.fs
+++ src/Maysternya/UIUtilities.fs
@@ -37,3 +37,18 @@ type ValuesEqualMultiConverter() =
             if values.Count > 1
             then values[0] = values[1]
             else false
+
+type EmptyStringToBoolConverter(inverted: bool) =
+    static member IsEmpty = EmptyStringToBoolConverter(inverted=false) :> IValueConverter
+    static member IsNotEmpty = EmptyStringToBoolConverter(inverted=true) :> IValueConverter
+    
+    interface IValueConverter with
+        member this.Convert(value: obj, targetType: Type, parameter: obj, culture: CultureInfo) =
+            let inverted = System.Convert.ToBoolean(parameter)
+            
+            match value with
+            | :? string as s -> String.IsNullOrWhiteSpace(s) = inverted
+            | _ -> not inverted
+            
+        member this.ConvertBack(value: obj, targetType: Type, parameter: obj, culture: CultureInfo) =
+            raise (NotSupportedException())
